e69d64762af374d78f04662418436854b98d11a3
robinsax <robin@stonebreakerdesigns.com>
2019-08-09 17:43:09 -0700

    copy default apps

--- setup.sh
+++ setup.sh
@@ -101,6 +101,9 @@ ln -s /work/source/libs/datazoomer/setup/www/static/dz /work/web/www/static
 ln -s /work/source/libs/datazoomer/setup/www/index.py /work/web/www
 chmod +x /work/source/libs/datazoomer/setup/www/index.py
 
+# copy apps
+cp -r /work/source/libs/datazoomer/apps /work/web/apps
+
 # create initial database
 service mysql start
 echo "create database zoomdata" | mysql -u root -proot
